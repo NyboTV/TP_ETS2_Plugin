e6d46b1955e188dda246a37d18e32fd682c2aa4d
NyboTV <nico.eichhorn1@web.de>
2021-06-12 13:29:52 +0200

    Update 0.5.3

--- src/updater.js
+++ src/updater.js
@@ -7,93 +7,75 @@ const exec = require('child_process').execFile;
 const AdmZip = require("adm-zip");
 const replace = require('replace-in-file');
 
-var user = 'NyboTV'
-var repo = 'TP_ETS2_Plugin'
-var outputdir = './tmp'
-var leaveZipped = false
-
 let firstStart = 1
 let Version = ""
 let config = ""
 
 config = fs.readFileSync('./config.json')
 config = JSON.parse(config)
+    
 
-if(config.autoupdate === "true") {
-    
-    if(fs.existsSync('./update.bat')) {
-        fs.rmSync('./update.bat')
-    }
-    
-    logIt("INFO", "Searching new Version")
-    
-    const options = {
-        hostname: 'api.github.com',
-        port: 443,
-        path: '/repos/NyboTV/TP_ETS2_Plugin/releases/latest',
-        method: 'GET',
-        headers: {'user-agent': 'node.js'}
-    }
-    
-    fs.writeFileSync('./updater.json', " ")
-    https.get(options, (res) => {
-        
-        res.on('data', (d) => {
-            fs.appendFileSync('./updater.json', d)
-        });
-        
-    }).on('error', (e) => {
-        console.error(e);
-    })
-    
-    setTimeout(() => {
-        Version = fs.readFileSync("./updater.json", 'utf-8')
-        Version = JSON.parse(Version).tag_name
-        fs.unlinkSync('./updater.json')
-        
-        
-        
-        if(fs.existsSync('./tmp')) {
-            fs.rmdirSync('./tmp', { recursive: true })
-            fs.mkdirSync('./tmp')
-        } else {
-            fs.mkdirSync('./tmp')
-        }
-        
-        console.log("Online Version: " + Version + " | Lokale Version: " +  config.version)
-        
-        if (config.version === Version) {
-            logIt("INFO", "Up to Date!")
-            Start()
-        } else {
-            logIt("INFO", "New Version Found! Plugin gets Updated after restart!")
-            
-            const options = {
-                files: './config.json',
-                from: `${data.updateLatest}`,
-                to: `yes`,
-            };
-            
-            try {
-                var updateLatest = replace.sync(options);
+logIt("INFO", "Searching new Version")
 
-                if(updateLatest[0].hasChanged === true) {
-                    logIt("INFO", "Update Latest has been changed!")
-                }
-            }
-            catch (error) {
-                logIt("ERROR", error)
-            }   
-            
-        }
-        
-    }, 1500);
-    
-} else {
-    Start()
-    return;
+const options = {
+    hostname: 'api.github.com',
+    port: 443,
+    path: '/repos/NyboTV/TP_ETS2_Plugin/releases/latest',
+    method: 'GET',
+    headers: {'user-agent': 'node.js'}
 }
+
+fs.writeFileSync('./tmp/updater.json', " ")
+https.get(options, (res) => {
     
+    res.on('data', (d) => {
+        fs.appendFileSync('./tmp/updater.json', d)
+    });
+    
+}).on('error', (e) => {
+    console.error(e);
+})
+
+setTimeout(() => {
+    Version = fs.readFileSync("./tmp/updater.json", 'utf-8')
+    Version = JSON.parse(Version).tag_name
+    fs.unlinkSync('./tmp/updater.json')
+    
+    fs.writeFileSync('./tmp/update.json', `{ \n"newVersion": "${Version}" \n}`)
+    
+    console.log("Online Version: " + Version + " | Lokale Version: " +  config.version)
+    
+    if (config.version === Version) {
+        logIt("INFO", "Up to Date!")
+        Start()
+    } else {
+        logIt("INFO", "New Version Found! Plugin gets Updated after restart!")
+        var data = fs.readFileSync('./config.json')
+        data = JSON.parse(data)
+        
+        const options = {
+            files: './config.json',
+            from: `${data.updateLatest}`,
+            to: `yes`,
+        };
+        
+        try {
+            var updateLatest = replace.sync(options);
+            
+            if(updateLatest[0].hasChanged === true) {
+                logIt("INFO", "Update Latest has been changed!")
+            }
+            Start()
+        }
+        catch (error) {
+            logIt("ERROR", error)
+        }   
+        
+    }
+    
+}, 1500);
+
+
 function Start() {
     exec('ets2_plugin.exe', function(err, data) {  
         logIt("ERROR", err)
