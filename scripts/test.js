ea421c0f4b958c9e320c894c859bcc980dd76799
NyboTV <46395862+NyboTV@users.noreply.github.com>
2022-01-31 01:19:56 +0100

    PreUpdate for Reworked Version

--- scripts/test.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-const { exit } = require('process')
-
-fs.copyFileSync('./src/config.json', './config.json')
-fs.copyFileSync('./src/userSettings.json', './userSettings.json')
-fs.copyFileSync('./src/entry.tp', './entry.tp')
-
-exit
